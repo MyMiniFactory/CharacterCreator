9f6ebcb51b5331be32d9594b3bed1afe59bb42bd
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-02-13 13:59:54 +0000

    tmp commit to delete this branch

--- src/three/loadedObjectsManager/manager.js
+++ src/three/loadedObjectsManager/manager.js
@@ -1,70 +1,131 @@
-import { Matrix4 } from 'three'
+import { Matrix4, Group, Object3D, Bone } from 'three'
 
 import defaultCategories from './categories'
+import Category, { ParentCategory } from './category'
 
 /**
  * 
  */
 class GroupManager {
+    /**
+     * @param { Group } group
+     */
     constructor( group, categories = defaultCategories ) {
+        
         this.group = group
-        // this.categories = categories
-
-        this.categoriesMap = categories.reduce(
-            ( map, category ) => map.set( category.id, category ),
-            new Map
-        )
-
-        /** mapping from categoryName to loadedObject (initially filled with null) */
-        this.loadedObjectsMap = categories.reduce(
-            ( map, category ) => map.set( category.id, null ),
-            new Map
-        )
 
         /**
-         *  mapping from boneId to loadedObject (initially filled with null)
+         * A mapping from category ID to category data
+         * @type { Map< string, Category > }
+         */
+        this.categoriesMap = categories.reduce(
+            ( categoriesMap, category ) => categoriesMap.set( category.id, category ),
+            new Map
+        )
+
+
+        /**
+         * @type { Map< string, Object3D > }
+         * mapping from categoryName to loadedObject (initially filled with null)
+         */
+        this.loadedObjectsMap = new Map
+
+        /**
+         * @type { Map< string, Bone > }
+         *  mapping from boneId to loadedObject
          * 
          * Note: assumes bone names are unique
          */
-        this.bonesMap = categories.reduce(
-            ( map, category ) => {
-                const { attachmentBones } = category
-                attachmentBones.forEach( boneId => map.set( boneId, null ) )
-                return map
-            },
-            new Map
-        )
+        this.bonesMap = new Map;
     }
 
-    getBone( boneId ) {
-        return this.bonesMap.get( boneId )
-    }
-
-    setBone( boneId, value ) {
-        this.bonesMap.set( boneId, value )
-    }
 
     add( categoryId, object3d, metaData = {} ) {
         if ( ! this.categoriesMap.has( categoryId ) ) {
-            throw new Error( `Category ${categoryId} is not defined!` )
+            throw new Error( `Category ${ categoryId } is not defined!` )
         }
 
+        const category = this.categoriesMap.get( categoryId )
         
-        const currentObject = this.loadedObjectsMap.get( categoryId )
+        // const currentObject = this.loadedObjectsMap.get( categoryId )
         
-        const newBonesMap = this.getBonesMap( object3d )
-        if ( currentObject ) {
-            this.replace( categoryId, object3d, newBonesMap )
+        const knownBoneNames = new Set( category.attachmentBones )
+        const extractedBones = extractKnownBones( object3d, knownBoneNames )
+
+        if ( this.loadedObjectsMap.has( categoryId ) ) {
+
+            this.replace( category, object3d, extractedBones )
+
         } else {
-            this.place( categoryId, object3d )
+
+            this.place( category, object3d, extractedBones )
+
         }
         
-        // this.extractBones( object3d )
-        this.updateBones( newBonesMap )
         this.loadedObjectsMap.set( categoryId, object3d )
 
     }
 
+    /**
+     * 
+     * @param { Category } category 
+     * @param { Object3D } object3d 
+     * @param { Map< string, Bone > } extractedBones 
+     */
+    place( category, object3d, extractedBones ) {
+        const { attachmentBones, parent } = category
+
+        attachmentBones.forEach( boneId => {
+
+            const newBone = extractedBones.get( boneId )
+
+            this.bonesMap.set( boneId, newBone )
+
+        } )
+
+        this.getParent( parent ).add( object3d )
+
+    }
+
+    /**
+     * 
+     * @param { Category } category 
+     * @param { Object3D } object3d 
+     * @param { Map< string, Bone > } extractedBones 
+     */
+    replace( category, object3d, extractedBones ) {
+
+
+        category.attachmentBones.forEach( boneId => {
+
+            const oldBone = this.bonesMap.get( boneId )
+            const newBone = extractedBones.get( boneId )
+
+            this.bonesMap.set( boneId, newBone )
+
+
+
+            // this will automatically move the children to their new parent
+            newBone.add( ...oldBone.children )
+
+        } )
+        
+        const parent = this.getParent( category.parent )
+        // TODO
+        // remove current obj from parent
+        const currentObject = this.loadedObjectsMap.get( category.id )
+
+        parent.remove( currentObject )
+
+        // then add new object to parent
+        parent.add( object3d )
+        
+    }
+
+    /**
+     * 
+     * @param { ParentCategory } parentCategory 
+     */
     getParent( parentCategory ) {
         if ( parentCategory ) {
             const parentBone = this.bonesMap.get( parentCategory.boneName )
@@ -81,70 +142,12 @@ class GroupManager {
         }
     }
 
-    place( categoryId, object3d ) {
-
-        const category = this.categoriesMap.get( categoryId )
-
-        this.getParent( category.parent ).add( object3d )
-
-    }
-
-    replace( categoryId, object3d, bonesMap ) {
-
-        const category = this.categoriesMap.get( categoryId )
-
-        category.attachmentBones.forEach( boneId => {
-            const oldBone = this.bonesMap.get( boneId )
-            const newBone = bonesMap.get( boneId )
-
-            // this will automatically move the children to their new parent
-            newBone.add( ...oldBone.children )
-
-        } )
-        
-        const parent = this.getParent( category.parent )
-        // TODO
-        // remove current obj from parent
-
-        // then add new object to parent
-        
-    }
-
-    /**
-     * traverses the object and adds all sets all bones that are in the bonesMap;
-     * if the bone is not in the map, it is ignored
-     * @param {THREE.Object3D} object3d 
-     */
-    extractBones( object3d ) {
-        object3d.traverse( element => {
-            if ( element.isBone ) {
-                if ( this.bonesMap.has( element.name ) ) {
-                    this.bonesMap.set( element.name, element )
-                }
-            }
-        } )
-    }
-
     updateBones( newBonesMap ) {
         newBonesMap.forEach(
             ( value, key ) => {
                 this.bonesMap.set( key, value )
             }
         )
-    }
-
-    getBonesMap( object3d ) {
-        const map = new Map
-
-        object3d.traverse( element => {
-            if ( element.isBone ) {
-                if ( this.bonesMap.has( element.name ) ) {
-                    map.set( element.name, element )
-                }
-            }
-        } )
-
-        return map
     }
 
     getCategoryById( id ) {
@@ -159,9 +162,10 @@ class GroupManager {
         //     console.log( this.categories )
         // }
 
-        console.log("categories:", this.categoriesMap )
-        console.log("loadedBones:", this.bonesMap )
+        // console.log("categories:", this.categoriesMap )
+        // console.log("registeredBones:", this.registeredBonesSet)
         console.log("loadedObjects:", this.loadedObjectsMap )
+        console.log("loadedBones:", this.bonesMap )
     }
 }
 
@@ -174,6 +178,26 @@ function getChildWithCorrectMatrixWorld( child, parent ) {
     child.applyMatrix( new Matrix4().getInverse( parent.matrixWorld ) )
 
     return child
+}
+
+
+/**
+ * returns a map from boneId to Bone containing only the registered bones
+ * @param { Object3D } object3d
+ * @param { Set< string > } knownBoneNames
+ */
+function extractKnownBones( object3d, knownBoneNames ) {
+    const map = new Map
+
+    object3d.traverse( element => {
+        if ( element.isBone ) {
+            if ( knownBoneNames.has( element.name ) ) {
+                map.set( element.name, element )
+            }
+        }
+    } )
+
+    return map
 }
 
 export default GroupManager 
